d7e94bf8d74557ca6ff6c2aa67acf66ac63ac61f
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-06-16 17:05:30 +0000

    App renders JSON including file size

---
+++ routes.js
@@ -0,0 +1,13 @@
+"use strict";
+const express = require('express'),
+      router = express.Router(),
+      multer = require('multer'),
+      upload = multer({ dest: 'uploads/' });
+
+router.use(express.static('./public'));
+
+router.post('/get-file-size', upload.single('test'), function(req, res) {
+  res.json({ size: req.file.size });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require('express'),
+      app = express(),
+      router = require('./routes'),
+      portNumber = process.env.PORT || process.argv[2] || 8080;
+
+app.use('/', router);
+
+app.listen(portNumber, function () {
+  console.log("Listening on port " + portNumber);
+});
